f7063d94bbd1adcf5174c3be18e292d5af177818
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2020-07-16 15:59:42 +0100

    Updates

---
+++ precache-manifest.e813ce6fb094c90ccf4c334bba0ef1d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0888ed0b945dd083934e785db089758",
+    "url": "/zoo-game/index.html"
+  },
+  {
+    "revision": "0fb3b739840574e04b9e",
+    "url": "/zoo-game/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "28a33f2cedfe4dac278c",
+    "url": "/zoo-game/static/js/2.3c316f38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zoo-game/static/js/2.3c316f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb3b739840574e04b9e",
+    "url": "/zoo-game/static/js/main.e8ab2473.chunk.js"
+  },
+  {
+    "revision": "20080c0bc47b96a301d3",
+    "url": "/zoo-game/static/js/runtime-main.7cd42693.js"
+  }
+]);
